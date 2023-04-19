45d279c6472d5383d3f55adbb5685d99a3c890d4
Up-s <dldbdjq@gmail.com>
2023-04-19 20:49:28 +0900

    ADD Widget

---
+++ InWidget/InWidget.swift
@@ -0,0 +1,68 @@
+//
+//  InWidget.swift
+//  InWidget
+//
+//  Created by YouUp Lee on 2023/04/19.
+//
+
+import WidgetKit
+import SwiftUI
+import Intents
+
+struct Provider: IntentTimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+    }
+
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        completion(entry)
+    }
+
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let configuration: ConfigurationIntent
+}
+
+struct InWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
+
+struct InWidget: Widget {
+    let kind: String = "InWidget"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            InWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct InWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        InWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
---
+++ InWidget/InWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  InWidgetBundle.swift
+//  InWidget
+//
+//  Created by YouUp Lee on 2023/04/19.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct InWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        InWidget()
+        InWidgetLiveActivity()
+    }
+}
